6671ef61185cf262d3069a75f6fec46a7ba63f4c
aa-Teye <alexteyeametepey@gmail.com>
2026-02-06 00:54:14 +0000

    Finish Testing git push different commands

--- monitor.py
+++ monitor.py
@@ -55,4 +55,6 @@ if __name__ == "__main__":
         "https://docs.flutter.dev/learn/pathway"
     ]
     
-    
+    report = check_sites(targets)
+    log_results(report)
+    print("\n Site health logged to health_log.csv")
